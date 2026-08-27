36758c98dfee7ec8f823bde2ed0f2ff16c3eceb2
catflower67 <stefanodepropano@gmail.com>
2026-08-27 17:29:33 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const button = document.getElementById("helloButton");
+const message = document.getElementById("message");
+
+button.addEventListener("click", function () {
+  message.textContent = "Кнопка работает! 🎉";
+});
